ce26d74fbec7c4aeac9e36b2a86557f5364760b1
Юрий Щербанеско <oguretrix@yandex.ru>
2022-11-21 21:39:53 +0300

    ДОбавлена функция продолжения и закрытия программы.

--- Часть калькулятора.py
+++ Часть калькулятора.py
@@ -5,20 +5,30 @@ if ans == 'N':
         if Qw == '0':
             calc = input("Введите математическое действие:\n")
             print("Ответ: " + str(eval(calc)))
-            ex = input('Хотите продолжить?(Да/Нет)')
-            if ex == 'Нет' == 'нет'
-                print('Программа закроется через 5 секунд')
+            ex = input('Хотите продолжить?(Да/Нет):')
+            if ex == 'Нет'or ex == 'нет':
+                print('Программа закроется через 5 секунд!!!')
                 import time
                 time.sleep(5)
                 break
 
-            if ex == 'Да' or ex == 'да'
-
-
-
+            if ex == 'Да' or ex == 'да':
+                print('Нажмите Enter чтобы продолжить!')
+                input()
 
         if Qw == '@':
             b = input('Введите число:')
             a = input('Введите число процент которого хотите узнать:')
             c =float(a) / float(b) * 100
             print('ответ: ' +str(c))
+            ex = input('Хотите продолжить?(Да/Нет):')
+            if ex == 'Нет' or ex == 'нет':
+                print('Программа закроется через 5 секунд!!!')
+                import time
+
+                time.sleep(5)
+                break
+
+            if ex == 'Да' or ex == 'да':
+                print('Нажмите Enter чтобы продолжить!')
+                input()
